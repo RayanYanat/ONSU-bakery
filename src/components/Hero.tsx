a03848751997e2be26117a1b8a61a51105c587f5
RayanYanat <43473711+RayanYanat@users.noreply.github.com>
2026-04-18 20:12:25 +0800

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -76,7 +76,7 @@ export default function Hero() {
             </span>
             <span
               className="hero-word block"
-              style={{ animationDelay: '0.65s', color: '#B8965A', marginTop: '0.15em' }}
+              style={{ animationDelay: '0.65s', color: '#B8965A' }}
             >
               defies
             </span>
